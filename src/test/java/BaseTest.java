6b3a1e90e60708df6e62c78c3abc837164cd4746
Niladri Das <niladridas@Niladris-MacBook-Air.local>
2026-03-02 00:33:04 +0530

    yml modification

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -48,6 +48,7 @@ public class BaseTest {
             if (browsername.equalsIgnoreCase("chrome")) {
 
                 ChromeOptions options = new ChromeOptions();
+                options.setBinary("/usr/bin/chromium-browser");
                 options.addArguments("--headless=new");
                 options.addArguments("--no-sandbox");
                 options.addArguments("--disable-dev-shm-usage");
