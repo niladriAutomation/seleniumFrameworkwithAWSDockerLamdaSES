e89f8ef84977b9639a111181c71edc519994d927
Niladri Das <niladridas@Niladris-MacBook-Air.local>
2026-03-02 00:25:10 +0530

    yml modification

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -51,6 +51,8 @@ public class BaseTest {
                 options.addArguments("--headless=new");
                 options.addArguments("--no-sandbox");
                 options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
+
 
                 driver = new ChromeDriver(options);
             }
